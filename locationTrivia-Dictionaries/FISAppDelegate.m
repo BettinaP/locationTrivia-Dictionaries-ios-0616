ed27b2594555f8654f6a63a232a8746b5c9dbfa8
BettinaP <bettinaprophete@gmail.com>
2016-06-05 22:36:43 -0400

    located

--- locationTrivia-Dictionaries/FISAppDelegate.m
+++ locationTrivia-Dictionaries/FISAppDelegate.m
@@ -16,4 +16,80 @@
  
  */
 
+-(NSString *)stringByTruncatingNameOfLocation:(NSDictionary *)location toLength:(NSUInteger)length {
+    NSString *abbreviatedName = location[@"name"];//why didn't location[name] work to set new string to be evaluated as dictionary value, why [@"name"]?
+    
+  
+        if ([abbreviatedName length] > length){
+        abbreviatedName = [abbreviatedName substringToIndex:length];
+        NSLog(@"\n\n\n\n\n\n\n\n\n this is abbrev. name: %@", abbreviatedName);
+        }
+    
+    return abbreviatedName;
+
+}
+
+-(NSDictionary *)dictionaryForLocationWithName:(NSString *)name latitude:(CGFloat)latitude longitude:(CGFloat)longitude {
+    
+    NSDictionary *location = @{ @"name" : name,
+                                @"latitude" : @(latitude),
+                                @"longitude" : @(longitude)
+                             };
+                                
+    return location;
+    
+}
+
+
+-(NSArray *)namesOfLocations:(NSArray *)locations {
+    NSMutableArray *locationNames = [[NSMutableArray alloc]init];
+    
+    for (NSDictionary *location in locations){
+        NSString *name = location[@"name"];// why is it for NSDictionary location and why @"name", why was error message telling me that it needs to be initialized?
+        [locationNames addObject:name];
+    }
+    
+    return locationNames;
+    
+}
+
+
+-(BOOL)dictionaryIsValidLocation:(NSDictionary *)location {
+        if ([location count] != 3){
+            return NO;
+        }
+    
+    NSString *name = location[@"name"];
+    if (!name){
+        return NO;
+    }
+    
+    
+    NSNumber *latitude = location[@"latitude"];
+    if (!latitude){
+        return NO;
+    }
+    
+    NSNumber *longitude =location[@"longitude"];
+    if (!longitude){
+        return NO;
+    }
+    return YES;
+}
+
+
+-(NSDictionary *)locationNamed:(NSString *)name inLocations:(NSArray *)locations {
+    for(NSDictionary *location in locations){
+        NSString *locationName = location[@"name"];
+        if ([locationName isEqualToString:name]){
+            return location;
+        }
+    
+    }
+    return nil;
+    
+}
+
+
+
 @end
